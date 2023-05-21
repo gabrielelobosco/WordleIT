3873a4a0b10497026ab327b381400b210816b431
Lobby <54267726+LobbyLobs@users.noreply.github.com>
2023-05-21 19:16:55 +0200

    Version 1.0
    Here is Version 1.0 of my WordleIT. Lot of things to do yet but it works someway.

---
+++ game.php
@@ -0,0 +1,556 @@
+
+<html>
+    <head>
+        <title>WordleIT</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://requirejs.org/docs/release/2.3.5/minified/require.js"></script>
+    </head>
+    <body onload="d()">
+        <div class="nav">
+            <h3>P</h3>
+            <h1>WordleIT</h1>
+            <button name="theme" onclick="tchange()">
+                <i class="fa-solid fa-sun-bright fa-2xl" style="color: #f7f13b; position: absolute; left: 2;"></i>
+                <i class="fa-solid fa-moon fa-2xl" style="color: #dcdd21; position: absolute; right: 4;"></i>
+                <div id="circle"></div>
+            </button>
+        </div>
+
+        <div class="announcer">
+        
+        <h2 id="result"></h2>
+        <h2 id="word"></span></h2>
+
+        <div id="container">
+            <h3 id="first"></h3>
+            <h3 id="second"></h3>
+        </div>
+            
+        <button onclick="location.reload()">Continua!</button>
+
+        </div>
+
+        <?
+        $parole = file('5letters.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        $count = count($parole);
+        $rand = rand(0,$count-1);
+
+        $parola = strtoupper($parole[$rand]);
+        $lettere = array();
+
+        for($i = 0; $i <= 5; $i++) {
+            $lettere[$i] = substr($parola, $i, 1);
+        }
+        ?>
+        <div class="underline-container">
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+                <div class="break"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+                <div class="break"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+                <div class="break"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+                <div class="break"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+            <div id="underline"></div>
+                <div class="break"></div>
+        </div>
+
+    <form class="tastiera" id="input_form" method="post">
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="A">A</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="B">B</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="C">C</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="D">D</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="E">E</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="F">F</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="G">G</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="H">H</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="I">I</button>
+    <div class="break"></div>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="J">J</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="K">K</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="L">L</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="M">M</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="N">N</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="O">O</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="P">P</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="Q">Q</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="R">R</button>
+    <div class="break"></div>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="S">S</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="T">T</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="U">U</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="V">V</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="W">W</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="X">X</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="Y">Y</button>
+        <button type="submit" name="tasto" id="tasto" class="neutral" value="Z">Z</button>
+    </form>
+
+<script type="text/javascript"> // TASTIERA FISICA
+
+c = 0;
+ra = 0;
+r = 1
+
+window.addEventListener("keydown", function(e) {
+		var key = `${e.key}`.toUpperCase();
+        
+        event.preventDefault(); // EVITA IL REFRESH
+        var value = key; // Lettera cliccata da utente
+
+        var element = document.querySelectorAll("button[value="+key+"]");
+
+        var parola = "<?echo $parola?>"; // Parola da trovare
+        var lunghezza = "<?echo $lenght?>"; // Lunghezza parola
+        const lettere = Array.from(parola); // Array di lettere
+
+        let corrette = []; // Array posizioni lettere corrette
+        let presenti = []; // Array posizioni lettere presenti (in diversa posizione)
+        let errate = []; // Array posizioni lettere errate
+
+    elems = document.querySelectorAll("[id='underline']");
+    elems[c].setAttribute('data-before', value);
+
+    c++;
+
+    if(c%5==0) { // Sono state inserite tutte le lettere
+    l = 0;
+        for(var i=5*ra; i<5*r; i++) {
+
+            if(elems[i].getAttribute('data-before') == lettere[l]) { // CORRETTE
+
+                corrette.push(elems[i].getAttribute('data-before'));
+                elems[i].className = "right";
+            } else {
+                if (parola.indexOf(elems[i].getAttribute('data-before'))>-1) { // PRESENTI
+
+                    presenti.push(elems[i].getAttribute('data-before'));
+                    elems[i].className = "present";
+                } else {
+                    errate.push(elems[i].getAttribute('data-before')); // ERRATE
+
+                    elems[i].className = "error";
+                }
+            }
+        l++;
+        }
+
+        errate.forEach(function(errata) {
+            var el = document.querySelector("button[value="+errata+"]");
+            el.className = "error";
+        });
+        presenti.forEach(function(presente) {
+            var el = document.querySelector("button[value="+presente+"]");
+            el.className = "present";
+        });
+        corrette.forEach(function(corretta) {
+            var el = document.querySelector("button[value="+corretta+"]");
+            el.className = "right";
+        });
+
+        if(corrette.length == 5)
+            ann(1,r,parola);
+        else { 
+            if (r==5)
+            ann(0,null,parola);
+        }
+        
+        r++;
+        ra++;
+        errate.length = 0;
+        presenti.length = 0;
+        corrette.length = 0;
+    }
+});
+</script>
+
+<script type="text/javascript"> // TASTIERA SU SCHERMO
+$('button').on('click', function(){
+    event.preventDefault(); // EVITA IL REFRESH
+    var value = $(this).val(); // Lettera cliccata da utente
+
+    if(value != "") {
+
+    var parola = "<?echo $parola?>"; // Parola da trovare
+    var lunghezza = "<?echo $lenght?>"; // Lunghezza parola
+    const lettere = Array.from(parola); // Array di lettere
+    
+    let corrette = []; // Array posizioni lettere corrette
+    let presenti = []; // Array posizioni lettere presenti (in diversa posizione)
+    let errate = []; // Array posizioni lettere errate
+
+    elems = document.querySelectorAll("[id='underline']");
+    elems[c].setAttribute('data-before', value);
+
+    c++;
+
+    if(c%5==0) { // Sono state inserite tutte le lettere
+    l = 0;
+        for(var i=5*ra; i<5*r; i++) {
+
+            if(elems[i].getAttribute('data-before') == lettere[l]) { // CORRETTE
+
+                corrette.push(elems[i].getAttribute('data-before'));
+                elems[i].className = "right";
+            } else {
+                if (parola.indexOf(elems[i].getAttribute('data-before'))>-1) { // PRESENTI
+
+                    presenti.push(elems[i].getAttribute('data-before'));
+                    elems[i].className = "present";
+                } else {
+                    errate.push(elems[i].getAttribute('data-before')); // ERRATE
+
+                    elems[i].className = "error";
+                }
+            }
+        l++;
+        }
+
+        errate.forEach(function(errata) {
+            var el = document.querySelector("button[value="+errata+"]");
+            el.className = "error";
+        });
+        presenti.forEach(function(presente) {
+            var el = document.querySelector("button[value="+presente+"]");
+            el.className = "present";
+        });
+        corrette.forEach(function(corretta) {
+            var el = document.querySelector("button[value="+corretta+"]");
+            el.className = "right";
+        });
+
+        if(corrette.length == 5)
+            ann(1,r,parola);
+        else { 
+            if (r==5)
+            ann(0,null,parola);
+        }
+        
+        r++;
+        ra++;
+        errate.length = 0;
+        presenti.length = 0;
+        corrette.length = 0;
+    }
+    }
+});
+</script>
+
+    </body>
+</html>
+<style>
+.dark_theme {
+    --bc: #212121;
+    --tc: white;
+    --navbc: #1b1b1bbf;
+    --navbt: #2b2b2b;
+    --navbtc: #333333;
+    --annbc: #292929;
+    --fh: 'Itim', cursive;
+}
+
+.light_theme {
+    --bc: white;
+    --tc: black;
+    --navbc: grey;
+    --navbt: black;
+    --navbtc: yellow;
+    --annbc: green;
+    --fh: 'Itim', cursive;
+}
+
+html {
+    background-color: var(--bc);
+}
+.nav {
+    width: 100%;
+    height: 5rem;
+    background-color: var(--navbc);
+    top: 0;
+    text-align: center;
+    position: fixed;
+    left: 0;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+.nav button {
+    width: 4rem;
+    height: 2rem;
+    position: absolute;
+    right: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 2rem;
+    border: none;
+    background-color: var(--navbt);
+}
+.nav button #circle {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 100%;
+    background-color: var(--navbtc);
+    position: absolute;
+    transform: translateX(-6px);
+
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    animation-timing-function: cubic-bezier(0.5, 0.05, 0.25, 1);
+}
+h3 {
+    color: var(--tc);
+    position: absolute;
+    left: 1rem;
+}
+.nav h1 {
+    text-align: center;
+    color: var(--tc);
+    font-family: var(--fh);
+    font-size: 2rem;
+    right: 0;
+    left: 0;
+    position: absolute;
+}
+
+.underline-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 8rem;
+}
+#underline {
+    width: 3rem;
+    height: 3rem;
+    border: 1px solid grey;
+    margin: 5px;
+    font-size: 3rem;
+    color: var(--tc);
+    text-align: center;
+}
+#underline::before {
+    content: attr(data-before);
+}
+.tastiera {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    margin-top: 3rem;
+}
+#tasto {
+    margin-left: 0.5rem;
+    margin-top: 0.3rem;
+    width: 2.5rem;
+    height: 3rem;
+    border-radius: 0.3rem;
+    font-family: monospace;
+    font-size: 1.5rem;
+}
+
+#tastob {
+    margin-left: 0.5rem;
+    margin-top: 0.3rem;
+    width: 6rem;
+    height: 3rem;
+    border-radius: 0.3rem;
+    font-family: 'Patrick Hand', cursive;
+    font-size: 1.5rem;
+}
+
+.break {
+    flex-basis: 100%;
+    height: 0;
+}
+
+@media screen and (max-width: 768px) {
+    #tasto {
+        width: 2rem;
+        height: 2.5rem;
+        font-size: 1.2rem;
+    }
+}
+
+.error {
+    background-color: #fc3503;
+}
+
+.right {
+    background-color: #45fc03;
+}
+
+.present {
+    background-color: #fcb603;
+}
+
+.announcer {
+  width: 35rem;
+  height: 35rem;
+  background-color: var(--annbc);
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  border: 2px solid grey;
+  border-radius: 20px;
+  display: none;
+
+  animation: announcer 0.75s forwards;
+}
+
+@keyframes announcer {
+  from {
+    transform: translateY(-1rem);
+    opacity: 70%;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 100%;
+  }
+}
+
+.announcer #container {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.announcer #result {
+  text-align: center;
+  color: var(--tc);
+  font-family: var(--fh);
+  font-size: xx-large;
+}
+
+.announcer #word {
+  text-align: center;
+  color: var(--tc);
+  font-family: var(--fh);
+  font-size: xx-large;
+}
+
+.announcer #first,
+.announcer #second {
+  position: relative;
+}
+
+.announcer button {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 8rem;
+  height: 4rem;
+  margin-top: 7rem;
+  background-color: var(--navbtc);
+  border-radius: 20px;
+  border: 2px solid grey;
+  color: var(--tc);
+  text-transform: uppercase;
+  font-family: var(--fh);
+}
+
+@keyframes tchangel {
+    from {transform: translate(-6px);}
+    to {transform: translate(26px);}
+}
+@keyframes tchanged {
+    from {transform: translate(26px);}
+    to {transform: translate(-6px);}
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Itim&display=swap');
+</style>
+
+<script>
+
+function d() {
+    document.documentElement.className = "dark_theme";
+}
+
+function tchange() {
+    var c = document.getElementById("circle");
+
+    if (c.style.animationName == "") { // Se il bottone è in posizione default (CHIARO)
+        c.style.animationName = "tchangel";
+        change_th(1);
+    } else { 
+        if (c.style.animationName == "tchangel") { // Se il bottone è in posizione su scuro (SCURO)
+            c.style.animationName = "tchanged";
+            change_th(0);
+        } else { 
+            if (c.style.animationName == "tchanged") { // Se il bottone è in posizione su chiaro (CHIARO)
+                c.style.animationName = "tchangel";
+                change_th(1);
+            }
+        }
+    }
+}
+
+function change_th(theme) { // theme = 0 DARK , theme = 1 LIGHT
+var root = document.documentElement;
+    
+    if(theme==1) {
+        root.className = "light_theme";
+    } else {
+        root.className = "dark_theme";
+    }
+}
+
+function ann(res, rows, word) {
+  var a = document.querySelector(".announcer");
+  a.style.display = "initial";
+
+  document.getElementById("second").innerHTML = "PUNTI";
+  document.getElementById("word").innerHTML = "La parola era <span style='color:#2dbe0f'>" + word + "</span>";
+
+  if (res == 1) {
+    document.getElementById("result").innerHTML = "HAI <span style='color:green'>VINTO</span>!";
+    if (rows == 1)
+      document.getElementById("first").innerHTML = "Hai indovinato in <span style='color:green'>" + rows + "</span> tentativo";
+    else
+      document.getElementById("first").innerHTML = "Hai indovinato in <span style='color:yellow'>" + rows + "</span> tentativi";
+  } else {
+    document.getElementById("result").innerHTML = "HAI <span style='color:red'>PERSO</span>!";
+    document.getElementById("first").innerHTML = "Avevi <span style='color:red'>5</span> tentativi";
+  }
+
+  // Avvia l'animazione dopo un breve ritardo per permettere la visualizzazione corretta dei cambiamenti
+  setTimeout(function() {
+    a.style.animation = "announcer 0.75s forwards";
+  }, 100);
+}
+</script>
+
+<!-- Font Awesome links -->
+<link rel="stylesheet" data-purpose="Layout StyleSheet" title="Default" disabled href="/css/app-af6a05f42b013986b481566363f0186f.css?vsn=d">
+<link rel="stylesheet" data-purpose="Layout StyleSheet" title="Web Awesome" href="/css/app-wa-cc491567b46eab1188c6538ebc462e7d.css?vsn=d">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-solid.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-regular.css">
+<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/sharp-light.css">
